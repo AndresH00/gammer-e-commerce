31e82e0714cbdaeb1966d9db8c1d39b2b1d6a2e4
AndresH00 <andresh.00@hotmail.com>
2022-11-27 00:45:00 -0600

    Avance sistema productos

---
+++ src/screens/Product/Home.js
@@ -0,0 +1,26 @@
+import { StyleSheet, View, Text, ScrollView } from "react-native";
+import React from "react";
+import StatusBar from "../../components/StatusBar";
+import Search from "../../components/Search";
+import NewProducts from "../../components/Home/NewProducts";
+import colors from "../../styles/colors";
+
+export default function Home() {
+  return (
+    <>
+      <StatusBar backgroundColor={colors.bgDark} barStyle="light-content" />
+      <Search />
+      <ScrollView>
+        <NewProducts />
+      </ScrollView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
