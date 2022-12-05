0c514adf89ed148e46a2f832abfc99d281a989f2
AndresH00 <andresh.00@hotmail.com>
2022-12-05 01:19:28 -0600

    Sistema Favorites

--- src/components/Product/Favorite.js
+++ src/components/Product/Favorite.js
@@ -1,24 +1,69 @@
 import { StyleSheet } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Button } from "react-native-paper";
+import { size } from "lodash";
+import {
+  isFavoriteApi,
+  addFavoriteApi,
+  deleteFavoriteApi,
+} from "../../api/favorite";
+import useAuth from "../../hooks/useAuth";
 
 export default function Favorite(props) {
   const { product } = props;
+  const [isFavorite, setIsFavorite] = useState(undefined);
+  const [loading, setLoading] = useState(false);
+  const { auth } = useAuth();
+  useEffect(() => {
+    (async () => {
+      const response = await isFavoriteApi(auth, product._id);
+      if (size(response) === 0) setIsFavorite(false);
+      else setIsFavorite(true);
+    })();
+  }, [product]);
 
-  const addProductFavorite = () => {
-    console.log("Add to Favorite");
-    console.log(product.title);
+  const addFavorite = async () => {
+    if (!loading) {
+      setLoading(true);
+      try {
+        await addFavoriteApi(auth, product._id);
+        setIsFavorite(true);
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    }
   };
+
+  const deleteFavorite = async () => {
+    if (!loading) {
+      setLoading(true);
+      try {
+        await deleteFavoriteApi(auth, product._id);
+        setIsFavorite(false);
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    }
+  };
+
+  if (isFavorite === undefined) return null;
 
   return (
     <Button
       mode="contained"
-      contentStyle={styles.btnAddFavoritesContent}
+      contentStyle={
+        isFavorite
+          ? styles.btnDeleteFavoritesContent
+          : styles.btnAddFavoritesContent
+      }
       labelStyle={styles.btnLabel}
       style={styles.btn}
-      onPress={addProductFavorite}
+      onPress={isFavorite ? deleteFavorite : addFavorite}
+      loading={loading}
     >
-      Add to Favorite
+      {isFavorite ? "Delete Favorite" : "Add to Favorite"}
     </Button>
   );
 }
@@ -26,6 +71,10 @@ export default function Favorite(props) {
 const styles = StyleSheet.create({
   btnAddFavoritesContent: {
     backgroundColor: "#057b00",
+    paddingVertical: 5,
+  },
+  btnDeleteFavoritesContent: {
+    backgroundColor: "#c40000",
     paddingVertical: 5,
   },
   btnLabel: {
--- src/screens/Favorites.js
+++ src/screens/Favorites.js
@@ -1,18 +1,60 @@
 import { StyleSheet, View, Text } from "react-native";
-import React from "react";
+import React, { useState, useCallback } from "react";
+import { useFocusEffect } from "@react-navigation/native";
+import StatusBar from "../components/StatusBar";
+import { size } from "lodash";
+import colors from "../styles/colors";
+import { getFavoriteApi } from "../api/favorite";
+import useAuth from "../hooks/useAuth";
+import Search from "../components/Search";
+import FavoriteList from "../components/Favorites/FavoriteList";
+import ScreenLoading from "../components/ScreenLoading";
 
 export default function Favorites() {
+  const [products, setProducts] = useState(null);
+  const [reloadFavorites, setReloadFavorites] = useState(false);
+  const { auth } = useAuth();
+
+  useFocusEffect(
+    useCallback(() => {
+      setProducts(null);
+      (async () => {
+        const response = await getFavoriteApi(auth);
+        setProducts(response);
+      })();
+      setReloadFavorites(false);
+    }, [reloadFavorites])
+  );
+
   return (
-    <View style={styles.container}>
-      <Text>Estamos en Favorites</Text>
-    </View>
+    <>
+      <StatusBar backgroundColor={colors.bgDark} barStyle="light-content" />
+      <Search />
+      {!products ? (
+        <ScreenLoading text="Loading Favorites" />
+      ) : size(products) === 0 ? (
+        <View style={styles.container}>
+          <Text style={styles.title}>Favorites</Text>
+          <Text>There is no products in your favorites</Text>
+        </View>
+      ) : (
+        <FavoriteList
+          products={products}
+          setReloadFavorites={setReloadFavorites}
+        />
+      )}
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 19,
+    marginBottom: 5,
   },
 });
